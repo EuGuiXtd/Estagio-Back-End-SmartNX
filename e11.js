2b12ee7a8864f28c6768f009df8ab5ce066302c5
EuGuiXtd <guilhermedpjf@gmail.com>
2024-04-04 21:15:52 -0300

    feat:exercicio 11

---
+++ e11.js
@@ -0,0 +1,12 @@
+const peoples = require ('./utils').peoples;
+
+function sortPeoplesByAge (peoples) {
+    return peoples.sort((a, b) => {
+        if (a.idade === b.idade) {
+            return b.id - a.id;
+        }
+        return b.idade - a.idade;
+    });
+}
+
+console.log(sortPeoplesByAge(peoples)); // [ { id: 3, nome: 'matheus', sobrenome: 'garcia', idade: 28 }, { id: 1, nome: 'juca', sobrenome: 'da silva', idade: 42 }, { id: 2, nome: 'daniel', sobrenome: 'gonçalves', idade: 21 }, { id: 4, nome: 'gabriel', sobrenome: 'pinheiro', idade: 21 } ]
